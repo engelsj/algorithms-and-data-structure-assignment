0c786b56fcb29eaa46a242d3f5cdf743eb93f143
Jamison Engels <engelsj@tcd.ie>
2019-02-08 14:15:16 +0000

    Added quicksort and some helper methods

--- assignment1/src/SortComparison.java
+++ assignment1/src/SortComparison.java
@@ -48,8 +48,46 @@ import java.util.Random;
      */
     static double [] quickSort (double a[])
     {
-    
+    	quickSortRecursive(a, 0, a.length - 1);
+    	return a;
 
+    }
+    
+    public static void quickSortRecursive(double a[], int low, int high)
+    {
+    	if(high <= low)
+    		return;
+    	
+    	if(a[high] < a[low])
+    		exch(a, low, high);
+    	
+    	int lt = low + 1, gt = high - 1;
+    	int i = low + 1;
+    	while(i <= gt)
+    	{
+    		if(a[i] < a[low])
+    			exch(a, lt++, i++);
+    		else if(a[high] < a[i])
+    			exch(a, i, gt--);
+    		else
+    			i++;
+    	}
+    	
+    	exch(a, low, --lt);
+    	exch(a, high, ++gt);
+    	
+    	quickSortRecursive(a, low, lt - 1);
+    	if(a[lt] < a[gt])
+    		quickSortRecursive(a, lt + 1, gt - 1);
+    	quickSortRecursive(a, gt + 1, high);
+    	
+    }
+    
+    public static void exch(double a[], int i, int j)
+    {
+    	double swap = a[i];
+    	a[i] = a[j];
+    	a[j] = swap;
     }
     
 
@@ -67,11 +105,70 @@ import java.util.Random;
      * @param a: An unsorted array of doubles.
      * @return after the method returns, the array must be in ascending sorted order.
      */
-
-    static double[] mergeSortIterative (double a[]) 
+    
+    // reference taken from https://www.geeksforgeeks.org/iterative-merge-sort/
+     static double[] mergeSortIterative (double a[]) 
     {
+    	int curr_size;
     	
-	
+    	int left_start;
+    	
+    	for(curr_size = 1; curr_size <= a.length - 1; curr_size = 2 * curr_size)
+    		for(left_start = 0; left_start < a.length - 1; left_start += 2 * curr_size)
+    		{
+    			int mid = left_start + curr_size - 1;
+    			int right_end = Math.min(left_start + 2 * curr_size - 1, a.length - 1);
+    			merge(a, left_start, mid, right_end);
+    		}
+    	return a;
+    }
+    
+    static void merge(double a[], int left, int mid, int right)
+    {
+    	int i, j, k;
+    	int n1 = mid - 1 + 1;
+    	int n2 = right - mid;
+    	
+    	double L[] = new double[n1];
+    	double R[] = new double[n2];
+    	
+    	for(i = 0; i < n1; i++)
+    		L[i] = a[1 + i];
+    	for(j = 0; j < n2; j++)
+    		R[j] = a[mid + 1 + j];
+    	
+    	i = 0;
+    	j = 0;
+    	k = 1;
+    	
+    	while(i < n1 && j < n2)
+    	{
+    		if(L[i] <= R[j])
+    		{
+    			a[k] = L[i];
+    			i++;
+    		}
+    		else
+    		{
+    			a[k] = R[j];
+    			j++;
+    		}
+    		k++;
+    	}
+    	
+    	while(i < n1)
+    	{
+    		a[k] = L[i];
+    		i++;
+    		k++;
+    	}
+    	
+    	while(j < n2)
+    	{
+    		a[k]= R[j];
+    		j++;
+    		k++;
+    	}
     }
     
     
@@ -83,6 +180,7 @@ import java.util.Random;
      * @param a: An unsorted array of doubles.
      * @return after the method returns, the array must be in ascending sorted order.
      */
+    // reference taken from https://www.geeksforgeeks.org/iterative-merge-sort/
     static double[] mergeSortRecursive (double a[]) 
     {
     	if(a == null)
@@ -164,6 +262,17 @@ import java.util.Random;
     }
 
    
+    
+    private static boolean isSorted(double a[]) {
+        return isSorted(a, 0, a.length - 1);
+    }
+
+    private static boolean isSorted(double[] a, int lo, int hi) {
+        for (int i = lo + 1; i <= hi; i++)
+            if (a[i] < a[i-1]) 
+            	return false;
+        return true;
+    }
 
 
     public static void main(String[] args) {
