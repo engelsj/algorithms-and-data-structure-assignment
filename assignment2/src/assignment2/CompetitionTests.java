8aae2218baa0dfd0e201c33c092646659fa13f05
Jamison Engels <engelsj@tcd.ie>
2019-03-26 11:40:51 +0000

    Added all required files

---
+++ assignment2/src/assignment2/CompetitionTests.java
@@ -0,0 +1,22 @@
+package assignment2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CompetitionTests {
+
+    @Test
+    public void testDijkstraConstructor() {
+
+        //TODO
+    }
+
+    @Test
+    public void testFWConstructor() {
+        //TODO
+    }
+
+    //TODO - more tests
+    
+}
